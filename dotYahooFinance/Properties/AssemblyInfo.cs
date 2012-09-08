ae1aa8078e62ddfdf12b5d213be1f005a434121e
Vladyslav Chyzhevskyi <coirius@coirius.com>
2012-09-08 17:37:50 +0300

    init

---
+++ dotYahooFinance/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("dotYahooFinance")]
+[assembly: AssemblyDescription("Yahoo! Finance wrapper for .NET Framework")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("dotYahooFinance")]
+[assembly: AssemblyCopyright("Copyright © Vladyslav Chyzhevskyi, 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b343434d-77b4-42e0-ba28-5eeb4bf99c16")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
